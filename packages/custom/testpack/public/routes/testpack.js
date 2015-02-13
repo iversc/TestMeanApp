124cf57252ffdad6ad2b7fe3d130c8359cabd4d4
Chris Iverson <cj.no.one@gmail.com>
2015-02-13 16:39:29 -0600

    Got second view page working

--- packages/custom/testpack/public/routes/testpack.js
+++ packages/custom/testpack/public/routes/testpack.js
@@ -2,13 +2,14 @@
 
 angular.module('mean.testpack').config(['$stateProvider',
   function($stateProvider) {
-    $stateProvider.state('testpack example page', {
-      url: '/testpack/example',
-      templateUrl: 'testpack/views/index.html'
-    })
-    .state('testpack test page', {
-	    url: '/testpack/test',
-	    templateurl: 'testpack/views/test.html'
-    });
+    $stateProvider
+	    .state('testpack example page', {
+	      url: '/testpack/example',
+	      templateUrl: 'testpack/views/index.html'
+	    })
+	    .state('testpack test page', {
+		    url: '/testpack/test',
+		    templateUrl: 'testpack/views/test.html'
+	    });
   }
 ]);
